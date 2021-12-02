6b3e96d8a59260fb296ce0fb6ed989c87e6ba58a
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-12-02 13:35:19 -0800

    User model updates

---
+++ server/models/User.js
@@ -0,0 +1,14 @@
+const { userSchema, model } = require('mongoose');
+
+const userSchema = new userSchema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true
+    },
+});
