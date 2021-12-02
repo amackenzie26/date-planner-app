75a0b5e86aadd00d40f2f98fa540a21829c89824
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-12-02 14:02:28 -0800

    server.js init

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(exdpress.json());
+
+app.use(routes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+});
