685a6e7ae0aff8f539e01692b19f7745d289e724
Kiri <kirilynne@gmail.com>
2021-12-03 14:26:51 -0500

    add return content to Signup.js

--- client/date-planner/src/pages/Home.js
+++ client/date-planner/src/pages/Home.js
@@ -3,22 +3,26 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
 
+
+
+    return (
+        <div className="home-container">
+            <h1 className="welcome">Welcome to Rendez-Vous!</h1>
+
+            <div className="card login">
+                <h2 className="login-text">Login to your account</h2>
+                <form className="login-form">
+                    <input className="username-login">Username:</input>
+                    <input className="password-login">Password:</input>
+                    <button className="submit-button">Login</button>
+                </form>
+                <button className="signup-button">Click here to sign up for an account. It's free!</button>
+            </div>
+            <img className="logo"></img>
+            <h3 className="slogan">CATCHY SLOGAN HERE</h3>
+        </div>
+    )
+
 }
 
-return (
-    <div className="home-container">
-        <h1 className="welcome">Welcome to Rendez-Vous!</h1>
-
-        <div className="card login">
-            <h2 className="login-text">Login to your account</h2>
-            <form className="login-form">
-                <input className="username-login">Username:</input>
-                <input className="password-login">Password:</input>
-                <button className="submit-button">Login</button>
-            </form>
-            <button className="signup-button">Click here to sign up for an account. It's free!</button>
-        </div>
-        <img className="logo"></img>
-        <h3 className="slogan">CATCHY SLOGAN HERE</h3>
-    </div>
-)
+export default Home;
