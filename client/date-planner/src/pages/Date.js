23c9dae6712d391147fec38c0cdc280dfd20a9c4
Kiri <kirilynne@gmail.com>
2021-12-03 16:21:14 -0500

    add pseudocode to Date.js

---
+++ client/date-planner/src/pages/Date.js
@@ -0,0 +1,13 @@
+//first the user sees the four prompts
+//nav bar at top has Dashboard (replaces Plan A Date), Profile and Calendar
+
+//prompt 1 (swipe left or right)
+//prompt 2 (swipe left or right)
+//prompt 3 (swipe left or right)
+//prompt 4 (swipe left or right)
+
+//three suggestions pop up that fit the criteria (horizontal cards)
+    //Button under the three "Show Me More Ideas"
+    //Reload three new cards
+
+//Logo and Slogan still at bottom
