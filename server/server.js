d01f1917ca370116b68467bcd98cd3ae864f07ab
Alec Mackenzie <alec.mackenzie26@gmail.com>
2021-12-07 09:30:59 -0800

    mongodb config

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const uri = process.env.MONGODB_URI;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
