784625cdd6bb1d6c3287f9b77e5a6c786959a123
Kiri <kirilynne@gmail.com>
2021-12-06 21:31:41 -0500

    update nav to be uncickable when logged out

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import Auth from '../utils/auth';
+import { useHistory } from 'react-router';
 
 const Nav = () => {
+
+    const history = useHistory();
+
+    const handlePageChange = (page) => {
+        if (!Auth.loggedIn()) {
+            alert("You must log in!");
+        } else {
+            if (page === "prompts") {
+                history.push("/prompts")
+            }
+            if (page === "dashboard") {
+                history.push("/dashboard")
+            }
+        }
+    }
 
     const handleLogout = () => {
         Auth.logout()
     }
 
+    //<a href="https://calendar.google.com/" class="calendar-link" target="https://calendar.google.com/" onClick={() => handlePageChange("calendar")}>My Calendar</a>
+
     return (
         <header>
             <nav class='app-nav'>
-                <a href="/prompts" class="plan-link">Plan A Date</a>
-                <a href="/dashboard" class="dashboard-link">Dashboard</a>
-                <a href="https://calendar.google.com/" class="calendar-link" target="https://calendar.google.com/">My Calendar</a>
-                {Auth.loggedIn() && (<a href="#logout" class="logout-link" onClick={handleLogout}>Logout</a>)}
+                <a href="#" class="plan-link" onClick={() => handlePageChange("prompts")}>Plan A Date</a>
+                <a href="#" class="dashboard-link" onClick={() => handlePageChange("dashboard")}>Dashboard</a>
+                {Auth.loggedIn() && (<a href="#" class="logout-link" onClick={handleLogout}>Logout</a>)}
             </nav>
         </header>
     )
